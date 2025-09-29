135745635d535ec17e5c86e9e323203e3e98eec4
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-09-29 22:54:42 +0530

    Add new files and programs

---
+++ practicesheet1.java
@@ -0,0 +1,43 @@
+/* 
+import java.util.*;
+public class Program_1{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int sum = num1 + num2 + num3;
+        System.out.println("The sum of " +num1+" + "+num2+" + "+num3+" = "+sum);
+    }
+} */
+//Question 2 -> calculate cgpa
+/*
+import java.util.*;
+public class Program_2{
+    public static void main(String[] args){
+        int GP1 = 9;
+        int GP2 = 8;
+        int GP3 = 5;
+        
+        int C1 = 4;
+        int C2 = 6;
+        int C3 = 7;
+        
+        int sub1 = GP1 * C1;
+        int sub2 = GP2 * C2;
+        int sub3 = GP3 * C3;
+        
+        int TotalCredit = C1 + C2 + C3;
+        double CGPA = (double)(sub1+ sub2 + sub3)/TotalCredit;
+        System.out.println("CGPA: "+CGPA);
+    }
+} */// Question -> 5
+ /*
+ import java.util.*;
+public class Program_5{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter your number: ");
+        System.out.println(sc.hasNextInt());// ye statement btati hai ki input me int hai ya nhi
+    }
+} */
