fc9e1f644ee9bf13b8c6ec4a9248abdcefc8b056
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-09-30 18:09:29 +0530

    Errors and Exceptions

---
+++ errorsdemo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class errorsdemo{
+    public static void main(String[] args){
+
+        //Syntax Error
+        //int a = 10 -> missing semicolon
+        //a = 5; -> undeclared variable
+
+        //Logical Errors Demo
+        //W A P to print all prime numbers between 1 to 10;
+       /* Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+
+    
+        for(int i = sc.nextInt();i<+5;i++){
+            System.out.println(2*i+1);
+        } */
+
+        // Runtime Error
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter: ");
+        int p = sc.nextInt();
+        int a = 14;
+        System.out.println(a + p); 
+         System.out.println("Enter2: ");
+        int k = sc.nextInt();
+        System.out.println("Integer part of 1000 divided by k is "+1000/k);
+    }
+}
