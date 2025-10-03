9ad5a60e7290e9d9ce6efecfc3030ab7c2fcd44e
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-03 14:01:29 +0530

    Adding Nested try-catch

--- exception.java
+++ exception.java
@@ -1,31 +1,35 @@
 import java.util.Scanner;
-class Myexception extends Exception{
+class MyException extends Exception{
     @Override
     public String toString() {
-        //return super.toString() +"I am toString()";
-        //return "I am toString()";
-        return super.toString();
+        return super.toString() + "You entered the wrong id password and id name.";
+       // return "I am toString()";
+       // return super.toString(); 
     }
     @Override
     public String getMessage() {
-        //return super.getMessage() +"I am getMessage";
-       // return  "I am getMessage()";
-        return super.getMessage();
-    }
-    
+       return super.getMessage() + "Now you have only one chance to unlock your phone. if you fail this time your phone get locked for the next 20 mins ";
+      // return "I am getMessage() ";
+      //  return super.getMessage();
+    } 
 }
 public class exception {
     public static void main(String[] args){
+        int a;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value: ");
-        int a = sc.nextInt();
-        if (a<5) {
-            try {
-                throw new Myexception();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.out.println(e.toString());
-            }
+        a = sc.nextInt();
+       if(a<99){
+        try {
+            //throw new MyException();
+            throw new ArithmeticException("This is an Arithmetic Exception.");
+        } catch (Exception e) {
+            System.out.println(e. getMessage());
+            System.out.println(e. toString());
+            System.out.println(e);
+            e.printStackTrace();
         }
+        System.out.println("Finished");
+       }
     }
 }
