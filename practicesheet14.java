2d0c500d6fe8cc2df3db4a6f8baa303ee6b2eab5
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-05 10:17:14 +0530

    Practice sheet 6

---
+++ practicesheet14.java
@@ -0,0 +1,11 @@
+public class practicesheet14 {
+    public static void main(String[] args){
+        //Question-1 -> WAP in java to demonstrate syntax,logical and runtime error.
+        //SYNTAX ERROR
+        int a = 40;
+        System.out.println("Y: "+a);
+        //
+
+
+    }
+}
---
+++ practicesheet6.java
@@ -0,0 +1,67 @@
+
+// Array
+//Exercise And Practice sheet
+
+// Example Program 1
+/*
+import java.util.*;
+public class Array_Program1{
+    public static void main(String[] args){
+        int[] x=new int[2];
+        System.out.println(x);
+        System.out.println(x[0]);
+    }
+} */
+/*
+package com.company;
+
+import java.util.*;
+public class Array_Program1{
+    public static void main(String[] args){
+        int[] Marks = {10,20,30,40,50};
+        System.out.println(Marks);
+        System.out.println(Marks[0]+","+Marks[1]+","+Marks[2]+","+Marks[3]+","+Marks[4]);
+        // if we want to know the length of the array then we will get it by
+        System.out.println(Marks.length);
+        // we can display array in two ways but the first one is not recommended
+        System.out.println(Marks[0]);
+        System.out.println(Marks[1]);
+        System.out.println(Marks[2]);
+        System.out.println(Marks[3]);
+        System.out.println(Marks[4]);
+        // And the other way is for loop
+        for(int i=0; i<Marks.length; i++){
+            System.out.println(i);
+            System.out.println(Marks[i]);
+        }
+         // the quiz is to print the elements of an array in reverse order
+         for(int i=Marks.length-1; i>=0; i--){
+            System.out.println(Marks[i]);
+        }
+        // displaying the array with for each loop
+        for(int element: Marks){
+            System.out.println(element);
+        }
+    }
+}
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       /*
+       float[] num = new float[5];
+       num[0] = 75.5f;
+       num[1] = 76.5f;
+       num[2] = 82.5f;
+       num[3] = 70.5f;
+       num[4] = 65.5f;
+       int sum =0;
+       for(int i = 0;i<num.length;i++){
+           sum += num[i];
+       }
+       System.out.println("The Sum of all Array element is " +sum);
+       */
+       // Question 2
+       //Scanner sc = new Scanner(System.in);
+    //}
+//} 
