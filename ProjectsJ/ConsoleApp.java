342bdb8cc14d3b09fcd78daf38482e3ff363eb04
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-07 07:21:57 +0530

    Modified File

--- ProjectsJ/ConsoleApp.java
+++ ProjectsJ/ConsoleApp.java
@@ -1,5 +1,9 @@
 package ProjectsJ;
 
+import java.util.Scanner;
 public class ConsoleApp {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    }
 }
