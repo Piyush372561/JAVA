294b4f19d0b9172e340de0e91839d5353b27faa4
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-07 08:14:09 +0530

    Modified file

--- pacticesheet1.java
+++ pacticesheet1.java
@@ -1,19 +1,16 @@
-/* 
 import java.util.*;
-public class Program_1{
+
+public class pacticesheet1 {
     public static void main(String[] args){
+    //Question-1 -> Adding three numbers
         int num1 = 10;
         int num2 = 20;
         int num3 = 30;
         int sum = num1 + num2 + num3;
         System.out.println("The sum of " +num1+" + "+num2+" + "+num3+" = "+sum);
-    }
-} */
-//Question 2 -> calculate cgpa
-/*
-import java.util.*;
-public class Program_2{
-    public static void main(String[] args){
+
+
+    //Question-2 calculate cgpa
         int GP1 = 9;
         int GP2 = 8;
         int GP3 = 5;
@@ -29,15 +26,12 @@ public class Program_2{
         int TotalCredit = C1 + C2 + C3;
         double CGPA = (double)(sub1+ sub2 + sub3)/TotalCredit;
         System.out.println("CGPA: "+CGPA);
-    }
-} */// Question -> 5
- /*
- import java.util.*;
-public class Program_5{
-    public static void main(String[] args){
+
+    //Question-3 -> Checking number is integer or not ?
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Enter your number: ");
         System.out.println(sc.hasNextInt());// ye statement btati hai ki input me int hai ya nhi
+    
     }
-} */
+}
