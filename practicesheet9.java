b6907f37830163119777cd9a073ff277493dcbe8
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-09 16:07:19 +0530

    Adding practice sheet 8

---
+++ practicesheet9.java
@@ -0,0 +1,58 @@
+//this and super keyword
+//this -> is a reference of an object ki uss class ke object ko class ke andar se kaise access kr  skte hai so thats why we use this keyword 
+//super keyword -> ka use krke hum constructors or methods koii bhi particular ko access kr skte hai duusri classes ke 
+/* class Program1{
+    int i;
+    Program1(int a){
+        this.i = a;
+    }
+     public int getI(){
+        return i;
+    }
+
+    Program1(){
+        System.out.println("I am a Constructor. ");
+    }
+}
+class Program2 extends Program1{
+    Program2(int c){
+        super(c);
+        System.out.println("I am a Program2 Constructor. ");
+    }
+}
+class A1{
+    public int a;
+    public int meth1(){
+        return 4;
+    }
+    public void meth2(){
+      System.out.println("I am a meth2 of class A1. ");  
+    }
+}
+class B1 extends A1{
+    @Override
+    public void meth2(){
+        System.out.println("I am a meth2 of class B1. ");
+    }
+    public void meth3(){
+        System.out.println("I am a meth3 of class B1. ");
+    }
+}
+class Phone{
+    public void greetings(){
+        System.out.println("Hi! I am your AI ");
+    }
+    public void on(){
+        System.out.println("Turning your Phone on........");
+    }
+}
+class SmartPhone extends Phone{
+    public void helloworld(){
+        System.out.println("Hello World.");
+    }
+    public void on(){
+        System.out.println("Turning your SmartPhone on........");
+    }
+
+} */
+
