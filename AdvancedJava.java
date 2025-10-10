7f37ae8ba40ff8422a2f24fc42ed1347d8ff73fa
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-10 18:56:19 +0530

    A program

--- AdvancedJava.java
+++ AdvancedJava.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+
 public class AdvancedJava {
     public static void main(String[] args){
+        ArrayList<Integer> I = new ArrayList<>();
+        ArrayList<Integer> I2 = new ArrayList<>();
+        I.add(3);
+        I.add(6);
+        I.add(9);
+        I.add(12);
+        I.add(15);
 
+        I2.add(2);
+        I2.add(4);
+        I2.add(6);
+        I2.add(8);
+        I2.add(10);
+        
+        I.addAll(0,I2);
+        I.set(1,40);
+        System.out.println(I.contains(41));
+        System.out.println(I.indexOf(6));
+        System.out.println(I.lastIndexOf(6));
+       // I.add(0,1);
+        for(int i = 0;i<I.size();i++){
+            System.out.print(I.get(i));
+            System.out.print(", ");
+        }
     }
 }
