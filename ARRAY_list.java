e32bb4be8decf2963543cb5d013441712aec69b5
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-13 07:29:04 +0530

    Adding Modification

--- ARRAY_list.java
+++ ARRAY_list.java
@@ -1,16 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class ARRAY_list {
     public static void main (String[] args){
         //ARRAYLIST 
 
-        List<String> Car = new ArrayList<>();
+        //List<String> Car = new ArrayList<>();
 
         //var Car = new ArrayList<String>(); 
 
-        //ArrayList<String> Car = new ArrayList<>();
+        ArrayList<String> Car = new ArrayList<>();
         Car.add("Ford");
         Car.add("Hyundai");
         Car.add("Maruti Suzuki");
@@ -20,15 +19,32 @@ public class ARRAY_list {
         //Car.set(0,"Scorpio");
         //Car.remove(2);
         //Car.clear();
+        //Car.contains("Thar");
          //Sorting of an ArrayList
         Collections.sort(Car);
+        //System.out.println(Car.contains("Thar"));
+        
+
 
         //for(int i = 0;i<Car.size();i++){
         //    System.out.println(Car.get(i));
         //}
+        if (Car.contains("Thar")) {
+            System.out.println("Yes!!! We have Thar.");
+        } else{
+            System.out.println("Sorry! Sir We don't Sell Mahindra Cars.");
+        }
+
         for(String i : Car){
            System.out.println(i);
         }
         //System.out.println(Car);
+        ArrayList<String> ClonedList = (ArrayList<String>) Car.clone();
+        ClonedList.add("Thar");
+        ClonedList.add("Nano");
+        for(int i = 0;i<ClonedList.size();i++){
+            System.out.println(ClonedList.get(i));
+
+        }
     }
 }
--- ArrayDeque.java
+++
@@ -1,5 +0,0 @@
-public class ArrayDeque {
-    public static void main(String[] args) {
-       
-    }
-}
