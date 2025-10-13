b80f3e97361579f340532e9fc98492614115c864
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-13 06:36:45 +0530

    Adding practice sheet 12

---
+++ practicesheet12.java
@@ -0,0 +1,51 @@
+//INTERFACE.JAVA
+/* class CellPhone{
+    public void Hiii(){
+        System.out.println("Hi! I AM  A CELL PHONE ");
+    }
+}
+interface GPS{
+    void gps();
+}
+interface Camera{
+    void camera();
+}
+interface MusicPlayer{
+    void  musicPlayer();
+}
+class SmartPhone extends CellPhone implements GPS ,Camera ,MusicPlayer{
+    public void smartphone(){
+        System.out.println("Yes! A cell phone can also be your SMARTPHONE.");
+    }
+    public void gps(){
+        System.out.println("I also works as a GPS.");
+    }
+    public void camera(){
+        System.out.println("OK, It's up to you. You can also use me as your camera.");
+    }
+    public void musicPlayer(){
+        System.out.println("If want to hear some Music just visit me. ");
+    }
+}
+public class Interface{
+   public static void main(String[] args){
+        System.out.print("When we ask a REDMI SmartPhone what features he possesses? He said - ");
+        /*SmartPhone redmi = new SmartPhone();
+        redmi.Hiii();
+        redmi.smartphone();
+        redmi.gps();
+        redmi.camera();
+        redmi.musicPlayer(); 
+        
+        //Polymorphism concept
+
+        GPS g = new SmartPhone();
+        g.gps();
+
+        Camera c = new SmartPhone();
+        c.camera();
+
+        MusicPlayer mp3 = new SmartPhone();
+        mp3.musicPlayer();
+   }
+} */
