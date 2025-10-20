6838b0e3fe603cf16b5c3724d4ae8d1984731bb2
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-20 10:10:33 +0530

    Doing some modifications

--- JavaReferences.java
+++ JavaReferences.java
@@ -1,11 +1,13 @@
 public class JavaReferences {
     public static void main(String[] args){
-        String str = "Piyush";;
-        System.out.println("The First name is "+str);
+        String str = "Piyush";
+        System.out.println("My name is "+str);
         //Java References
         System.out.println(str.charAt(4));
         System.out.println(str.codePointAt(2));
         System.out.println(str.codePointBefore(2));
         System.out.println(str.codePointCount(0,5));
+        System.out.println(str.compareTo("P"));
+        System.out.println(str.compareToIgnoreCase("P"));
     }
 }
