34a69c98701bc2025d7fa320d478b172c932f569
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-21 13:56:21 +0530

    CLass inherit

--- OOPS.java
+++ OOPS.java
@@ -1,6 +1,11 @@
+class Inherit{
+    public void Inherit() {
+      System.out.println("Hello!");
+    }
+}
 public class OOPS {
     public static void main(String[] args){
-        
-
+        Inherit I = new Inherit();
+        I.Inherit();
     }
 }
