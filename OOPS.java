0a7d6b30acf6a4b0653c7300e175e9dc31df53c7
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-23 15:08:09 +0530

    Adding new Print Statement

--- OOPS.java
+++ OOPS.java
@@ -2,6 +2,7 @@ class Inherit{
     public void Inherit() {
       System.out.println("Hello!");
       System.out.println("My name is Piyush.");
+      System.out.println("It's been a while Since i code. ");
     }
 }
 public class OOPS {
