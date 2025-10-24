cc8b2e0e5aed99fb0bd7fe7c42e7b79581cbbf77
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-24 17:19:50 +0530

    modified file

--- AdvancedJava.java
+++ AdvancedJava.java
@@ -1,39 +1,38 @@
-import java.util.HashSet;
+import java.util.ArrayList;
 public class AdvancedJava {
     public static void main(String[] args){
-       /*  ArrayList<Integer> I = new ArrayList<>();
-        ArrayList<Integer> I2 = new ArrayList<>();
+        ArrayList<Integer> I = new ArrayList<>();
         I.add(3);
         I.add(6);
         I.add(9);
         I.add(12);
         I.add(15);
-
+        I.set(1,40);
+        System.out.println(I.contains(41));
+        System.out.println(I.indexOf(6));
+        System.out.println(I.lastIndexOf(6));
+        System.out.println(I.contains(15));
+        I.ensureCapacity(4);
+        System.out.p
+        // I.add(0,1);
+        ArrayList<Integer> I2 = new ArrayList<>();
         I2.add(2);
         I2.add(4);
         I2.add(6);
         I2.add(8);
         I2.add(10);
-        
         I.addAll(0,I2);
-        I.set(1,40);
-        System.out.println(I.contains(41));
-        System.out.println(I.indexOf(6));
-        System.out.println(I.lastIndexOf(6));
-       // I.add(0,1);
         for(int i = 0;i<I.size();i++){
             System.out.print(I.get(i));
             System.out.print(", ");
         }
-        System.out.println();
-        
         //ARRAY DEQUE
-        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        /* ArrayDeque<Integer> ad = new ArrayDeque<>();
         ad.add(7);
         ad.add(97);
         //ad.addFirst(76);
         System.out.println(ad.getFirst());
-        System.out.println(ad.getLast()); */
+        System.out.println(ad.getLast()); 
 
         //HASHING TECHNIQUE
         HashSet<Integer> myHashset = new HashSet<>(16 ,0.75f);
@@ -42,7 +41,7 @@ public class AdvancedJava {
         myHashset.add(90);
         myHashset.add(8);
         myHashset.add(10);
-        System.out.println(myHashset);
+        System.out.println(myHashset);*/
 
     }
 }
