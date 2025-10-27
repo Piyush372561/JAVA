fa003a34949726ddb6c91a043e262d2356ab3caf
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-27 15:41:04 +0530

    All practice set Questions solution

--- Practiceof_Advancedjava.java
+++ Practiceof_Advancedjava.java
@@ -1,6 +1,12 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
 public class Practiceof_Advancedjava {
     public static void main(String[] args) {
+        // PROBLEM 1
         ArrayList<String> Al = new ArrayList<>();
         Al.add("Ayush ");
         Al.add("Piyush");
@@ -13,5 +19,30 @@ public class Practiceof_Advancedjava {
         for(Object O: Al){
             System.out.println(O);
         }
+        //PROBLEM 2
+        Date d = new Date();
+        System.out.println(d.getHours()+ ":" +d.getMinutes()+ ":" +d.getSeconds());
+
+        //PROBLEM 3
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.get(Calendar.HOUR)+ ":" +c.get(Calendar.MINUTE)+ ":" +c.get(Calendar.SECOND));
+        System.out.println(c.get(Calendar.HOUR_OF_DAY)+ ":" +c.get(Calendar.MINUTE)+ ":" +c.get(Calendar.SECOND));
+
+        //PROBLEM 4
+        LocalDate ld = LocalDate.now();
+        DateTimeFormatter Df = DateTimeFormatter.ofPattern("H:m:s");
+        String date = ld.format(Df);
+        System.out.println(date);
+        //PROBLEM 5
+        HashSet<Integer> s = new HashSet<>();
+        s.add(2);
+        s.add(4);
+        s.add(6);
+        s.add(7);
+        s.add(8);
+        s.add(2);
+        System.out.println(s);
+        
+
     }
 }
