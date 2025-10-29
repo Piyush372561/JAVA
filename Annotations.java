a57dba103299e112267b0380871b54de56fb94d2
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-29 17:37:50 +0530

    Trying to write an annotations

--- Annotations.java
+++ Annotations.java
@@ -1,5 +1,14 @@
+class MorningGreetings{
+
+}
+class Greetings extends MorningGreetings{
+    public void wish(){
+        System.out.println("Good Morning Sir!");
+    }
+}
 public class Annotations {
     public static void main(String[] args){
-
+        Greetings Mg = new Greetings();
+        Mg.wish();
     }
 }
