8e9c528759b459f510d098d3a8e5b5223580a256
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-30 18:46:01 +0530

    I learn to Create, Read, Write a file

--- FileHandling.java
+++ FileHandling.java
@@ -1,13 +1,44 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 public class FileHandling {
     public static void main(String[] args) {
-       File myfile = new File("1st Java File");
+        //Code to create a File.
+        File myfile = new File("1st Java File");
         try {
           myfile.createNewFile(); 
         } 
         catch (IOException e) {
           e.printStackTrace();
         }
+
+        //Code to Write a File.
+        try {
+            FileWriter myFilewriter = new FileWriter("1st Java File");
+            myFilewriter.write("Hello! EveryOne.\n");
+            myFilewriter.write("My name is Piyush Kushwaha.\nI am your Java teacher.");
+
+            myFilewriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Code to read a file
+        try {
+            File myFile = new File("1st Java File");
+            Scanner sc = new Scanner(myFile);
+
+            while(sc.hasNextLine()){
+                String str = sc.nextLine();
+                System.out.println(str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+
+        
     }
 }
