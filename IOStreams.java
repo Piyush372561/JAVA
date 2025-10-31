0d7bb30607d18cba5aea0ee8c6aa43119506f3ca
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-31 14:52:10 +0530

    This program Gives Error I don't know why can Anyone please help me

--- IOStreams.java
+++ IOStreams.java
@@ -1,5 +1,18 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 public class IOStreams {
     public static void main(String[] args){
-          
+        try {
+            FileInputStream fis = new FileInputStream("1stJavaFile.txt");
+            int i;
+
+            while((i = fis.read()) != -1){
+                System.out.println((char) i);
+            }
+            fis.close();
+        } catch (IOException e) {
+                e.printStackTrace(); 
+                System.out.println("Error in reading file. ");
+        }
     }
 }
