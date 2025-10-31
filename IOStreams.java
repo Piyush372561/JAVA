0d4b87cdd84c61c3f5b6f809f1391829db9a1a7e
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-10-31 15:20:40 +0530

    Trying to read my file

--- IOStreams.java
+++ IOStreams.java
@@ -1,15 +1,24 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 public class IOStreams {
+    
     public static void main(String[] args){
-        try {
-            FileInputStream fis = new FileInputStream("1stJavaFile.txt");
-            int i;
+        System.out.println("Java is running in this folder:");
+        System.out.println(System.getProperty("user.dir")); // <-- current working directory
+        
+        File file = new File("1stJavaFile.txt");
+        System.out.println("Absolute path: " + file.getAbsolutePath());
+        System.out.println("File exists? " + file.exists());
+        
+        
+        System.out.println(new java.io.File("1stJavaFile.txt").getAbsolutePath());
 
+        try(FileInputStream fis = new FileInputStream("C:\\Users\\kushw\\OneDrive\\Desktop\\CODING\\JAVA\\1stJavaFile.txt")) {
+            int i;
             while((i = fis.read()) != -1){
                 System.out.println((char) i);
             }
-            fis.close();
         } catch (IOException e) {
                 e.printStackTrace(); 
                 System.out.println("Error in reading file. ");
