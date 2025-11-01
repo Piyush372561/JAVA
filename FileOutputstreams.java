7c98342ae5d199393a7a4fd373aca590f809e390
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-01 08:01:23 +0530

    Adding Some comment for better understanding

--- FileOutputstreams.java
+++ FileOutputstreams.java
@@ -3,14 +3,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 public class FileOutputstreams {
     public static void main(String[] args) {
+        //This is the  text we want to write.
         String txt = "Hello World! ";
+
+        //Using try-with resources: it closes the file automatically
         try {
             FileOutputStream fos = new FileOutputStream("1stJavaFile");
+            //converts text into raw byte and then write
             fos.write(txt.getBytes());
             System.out.println("Successfully wrote to file.");
         } 
         catch(IOException e ){
             e.printStackTrace();
+            System.out.println("Error in writing File.");
         }
     }
 }
