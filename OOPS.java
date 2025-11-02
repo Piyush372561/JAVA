ebe0c8ebcd83b89dbbdd0a35444a4bc88adc3897
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-02 10:56:32 +0530

    A basic oops program

--- OOPS.java
+++ OOPS.java
@@ -1,9 +1,15 @@
 // THIS OOPS FILE WILL INCLUDE ALL PRACTICE PROBLEMS.
 // AUTHOR : PIYUSH KUSHWAHA.
-
+class Bike{
+    String B1 = "Harley Davidson X440";
+    String B2 = "Hunter 450";
+}
 public class OOPS {
     public static void main(String[] args){
         //GREETINGS.
         System.out.println("HEllo! GitHub this is my OOPS class.");
+        Bike moBikes = new Bike();
+        System.out.println("My First Favourite bike: "+moBikes.B1);
+        System.out.println("My Second Favourite bike: "+moBikes.B2);
     }
 }
