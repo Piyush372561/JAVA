81cb35052cb991576a01f0f40557aa7ba3b2b0eb
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-02 10:23:28 +0530

    A new BufferedReader file Program

---
+++ BufferdRW.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferdRW {
+    public static void main(String[] args) {
+        try(BufferedReader BR = new BufferedReader(new FileReader("1stJavaFile.txt"))){
+            String line;
+
+            while((line = BR.readLine()) != null){
+                System.out.println(line);
+            }
+        } 
+        catch(IOException e){
+            e.printStackTrace();
+            System.out.println("Error Occurs While Reading. ");
+        }
+    }
+}
