2196119ffa90beca3f232630bc4a695901473e1b
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2025-11-05 10:46:00 +0530

    Problem 1

---
+++ Ultimatepracticesheet.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ultimatepracticesheet {
+    public static void main(String[] args){
+       //Problem-1
+       Scanner sc = new Scanner(System.in);
+
+       System.out.print("Num1: ");
+       int num1 = sc.nextInt();
+       System.out.print("Num2: ");
+       int num2 = sc.nextInt();
+
+       int Sum = num1 + num2;
+
+       System.out.println("Sum of "+num1+" and "+num2+" = "+Sum);
+
+    }
+}
