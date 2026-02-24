a641f167133823e82f731b7bffd6ffbacb7d18d7
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-02-24 21:00:42 +0530

    Updated file

--- Linked_List.java
+++ Linked_List.java
@@ -1,6 +1,14 @@
+import java.util.LinkedList;
+
 public class Linked_List {
     public static void main(String[] args){
-        System.out.println("Linked List");
+        System.out.print("LinkedList: ");
         
+        LinkedList<Integer> Llist = new LinkedList<>();
+        Llist.add(1);
+        Llist.add(2);
+        Llist.add(3);
+
+        System.out.println(Llist);
     }
 }
--- com_parator.java
+++ com_parator.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-/*class Student{
+import java.util.List;
+
+ 
+class Student{
     String name;
     int age;
 
@@ -16,7 +20,7 @@ import java.util.Comparator;
     public int getAge() {
         return age;
     }
-}
+} 
 
 class StringLengthComparator implements Comparator<String> {
     @Override
@@ -28,30 +32,44 @@ class StringLengthComparator implements Comparator<String> {
         // Sort in descending order by length
         //return s2.length() - s1.length(); 
     }
-} */
+} 
 
 class MyComparator implements Comparator<Integer> {
 
     @Override
-    public int compare(Integer i1, Integer i2) {
+    public int compare(Integer a, Integer b) {
 
         // Sort in ascending order
-        //return i1 - i2; 
+        return a - b; 
        
        // Sort in descending order
-        return i2 - i1; 
+       // return b - a; 
     }
+    /* -ve aaya toh -> a pehle aayega
+    +ve aaya toh -> b pehle aayega
+    0 aaya toh -> dono equal hain */
 }
         
 public class com_parator {
     public static void main(String[] args) {
         System.out.println("Comparator");
 
-        /*ArrayList<Student> students = new Arraylist<>();
+        List<Student> students = new ArrayList<>();
         students.add(new Student("Piyush", 20));
         students.add(new Student("Micky", 22));
         students.add(new Student("Avi", 19));
-        students.sort(null); */
+
+        //Comparator<Student> com = Comparator.comparing(Student::getAge).reversed().thenComparing(Student::getName);
+
+         students.sort((s1, s2) -> {
+            if(s2.getAge() - s1.getAge() > 0) {
+                return 1; 
+            } else if (s2.getAge() - s1.getAge() < 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });  
 
 
         ArrayList<Integer> list = new ArrayList<>();
@@ -61,10 +79,10 @@ public class com_parator {
         list.add(3);
 
         //Ye sort method list ke elements ko sort kar deta hai, aur null pass karne par natural order me sort karta hai.
-        list.sort(new MyComparator());
-        System.out.println(list);
+        //list.sort(new MyComparator());
+        //System.out.println(list);
 
-        /*  Sort in ascending order
+        //Using lambda expression to sort the list of integers in ascending order
         list.sort((i1, i2) -> i1 - i2);
         System.out.println(list);
 
@@ -73,12 +91,13 @@ public class com_parator {
         System.out.println(list);
 
         List<String> words = Arrays.asList("Piyush", "Micky", "Avi");
+        //Using a custom comparator to sort the list of strings based on their length
+        words.sort(new StringLengthComparator());
+        System.out.println(words);
+
         //Using lambda expression to sort the list of strings based on their length
         words.sort((s1, s2) -> s1.length() - s2.length());
         System.out.println(words);
-
-        words.sort(new StringLengthComparator());
-        System.out.println(words);
-            */
+   
     }
 }
