421c8f1e2846dab014e906e76abd93c9cd7d4bec
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-02-26 16:16:03 +0530

    oops concept

--- OOPS.java
+++ OOPS.java
@@ -1,17 +1,7 @@
-// THIS OOPS FILE WILL INCLUDE ALL PRACTICE PROBLEMS.
-// AUTHOR : PIYUSH KUSHWAHA.
-class Bike{
-    String B1 = "Harley Davidson X440";
-    String B2 = "Hunter 450";
-}
-public class OOPS {
-    public static void main(String[] args){
-        //GREETINGS.
-        System.out.println("HEllo! GitHub this is my OOPS class.");
-        Bike moBikes = new Bike();
-        System.out.println("My First Favourite bike: "+moBikes.B1);
-        System.out.println("My Second Favourite bike: "+moBikes.B2);
-        //Inheritance 
-        
+// OOPS CONCEPTS IN JAVA
+
+class OOPS {
+    public static void main(String[] args) {
+        System.out.println("OOPS CONCEPTS IN JAVA");
     }
 }
