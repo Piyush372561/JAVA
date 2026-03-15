40fcab03ae49236c52bf8d2c0d5c125292ee5316
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-03-15 11:35:41 +0530

    updated file

--- Q4.java
+++ Q4.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= i; j++) {
-                int mat = sc.nextInt();
-                System.out.print(mat + " ");
+        int row = 0;
+        int col = 0;
+
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5; j++) {
+                int x = sc.nextInt();
+
+                if(x == 1) {
+                    row = i;
+                    col = j;
+                }
             }
-            System.out.println();
         }
+        int moves = Math.abs(row - 3) + Math.abs(col - 3);
+        System.out.println(moves);
     }
 }
